064d83179ad96ddaa7881a29b7fe0b60a9cbd69f
nckhrdy <nckhrdy@bu.edu>
2024-03-20 15:12:31 -0400

    FINAL VERSION

--- my-portfolio/src/components/About.tsx
+++ my-portfolio/src/components/About.tsx
@@ -4,20 +4,22 @@ const About: React.FC = () => {
   return (
     <div style={{ position: "relative" }}>
       {/* Content */}
-      <main className="grid grid-cols-6 grid-rows-6 h-screen" style={{ position: "relative", zIndex: 2 }}>
-        {/* Your existing content */}
-        <div className="col-start-1 col-end-3 row-start-1 row-end-2 flex flex-col justify-center items-center ml-20 mt-20">
+      <main className="grid grid-cols-1 md:grid-cols-6 grid-rows-3 md:grid-rows-6 min-h-screen" style={{ position: "relative", zIndex: 2 }}>
+        {/* Adjusted for responsiveness */}
+        <div className="col-start-1 md:col-start-1 md:col-end-3 row-start-1 md:row-start-1 row-end-2 flex flex-col justify-center items-center px-4 md:ml-20 mt-20">
           <p className="text-3xl md:text-4xl font-bold" style={{ fontFamily: "'DocumanSTC', serif", color: "#000000" }}>
             {"So! Who am I?"}
           </p>
         </div>
-        <div className="col-start-1 col-end-3 row-start-2 row-end-6 flex justify-center ml-20">
-          <img src="/assets/aboutme.svg" alt="Email Me!" />
+        {/* Image alignment adjusted */}
+        <div className="col-start-1 md:col-start-1 md:col-end-3 row-start-2 md:row-start-2 row-end-4 md:row-end-6 flex justify-center items-center px-4 md:ml-20">
+          <img src="/assets/aboutme.svg" alt="About Me" style={{ maxWidth: '100%', height: 'auto' }} />
         </div>
-        <div className="col-start-3 col-end-7 row-start-2 row-end-6 flex justify-center items-center mt-10 mr-20">
-          <div className="bg-white bg-opacity-90 shadow-lg p-4 md:p-6 max-w-3xl w-full rounded-lg" style={{ margin: "0 10%" }}>
+        {/* Text content area adjusted for responsiveness */}
+        <div className="col-start-1 md:col-start-3 md:col-end-7 row-start-3 md:row-start-2 row-end-4 md:row-end-6 flex justify-center items-center px-4 md:mt-10 md:mr-20">
+          <div className="bg-white bg-opacity-90 shadow-lg p-4 md:p-6 max-w-3xl w-full rounded-lg" style={{ margin: "0 auto" }}>
             <p className="text-lg md:text-2xl lg:text-3xl font-normal" style={{ fontFamily: "'Nunito', serif", color: "#000000", lineHeight: "1.6" }}>
-              {"I'm a senior studying Computer Engineering at Boston University and my biggest passion is innovation. My interests began with robotics from an early age and that has since evolved into a love for software development and entrepreneurship. Recently, most of my projects have been built using emebedded systems which has allowed me to quickly turn ideas to prototypes and then to practical solutions. I'm excited to see where my journey takes me as I aim to bridge the gaps between technology and business solutions."}
+              {"I'm a senior studying Computer Engineering at Boston University and my biggest passion is innovation. My interests began with robotics from an early age and that has since evolved into a love for software development and entrepreneurship. Recently, most of my projects have been built using embedded systems which has allowed me to quickly turn ideas to prototypes and then to practical solutions. I'm excited to see where my journey takes me as I aim to bridge the gaps between technology and business solutions."}
             </p>
           </div>
         </div>
---
+++ my-portfolio/src/pages/Project6/index.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from 'next/router';
+import Header from "../../components/Header";
+
+const Project1: React.FC = () => {
+  const textColor = "#575454";
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        <title>Personal Portfolio - Project 6</title>
+        <meta name="description" content="Project 1 description" />
+      </Head>
+      <Header />
+      <div
+        className="min-h-screen bg-gray-100 flex flex-col justify-center items-center"
+        style={{ fontFamily: "'DocumanSTC', serif", paddingTop: "100px" }} 
+      >
+        <div className="max-w-4xl mx-auto p-5">
+          {/* Back button */}
+          <button
+            onClick={() => router.back()}
+            className="mb-4 text-gray-600 hover:text-gray-800 transition duration-300 ease-in-out"
+            style={{ fontFamily: "'Nunito', sans-serif" }}
+          >
+            &larr; Back
+          </button>
+          <h1
+            className="text-4xl font-bold text-center mb-6"
+            style={{ color: textColor }}
+          >
+            My Portfolio
+          </h1>
+          <p className="text-lg mb-4 text-center" style={{ color: textColor }}>
+            My Personal Project Portfolio
+          </p>
+          <div className="w-full h-64 relative mb-8">
+            <Image
+              src="/assets/PortfolioHomePage.png"
+              alt="Project Image"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-lg"
+            />
+          </div>
+          <div className="bg-white shadow-md rounded-lg p-6">
+            <h2
+              className="text-2xl font-semibold mb-4"
+              style={{ color: textColor }}
+            >
+              Project Overview
+            </h2>
+            <p className="text-gray-600 pl-4 block"
+              style={{ fontFamily: "'Nunito', sans-serif" }}>
+              As a personal project, I wanted to demostrate a self taught skillset in web development. I built this website using Next.js, Tailwind CSS, and TypeScript and it is deployed using Vercel.I hope you enjoy the website and find it easy to navigate. If you have any feedback, feel free to reach out to me.
+              <br /><br />
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Project1;
