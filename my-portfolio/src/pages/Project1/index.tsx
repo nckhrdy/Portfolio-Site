33029d809a4478cc59e8c747c8f94ff8de1390c8
nckhrdy <nckhrdy@bu.edu>
2024-06-29 17:30:15 -0400

    upload test

--- my-portfolio/src/pages/Project1/index.tsx
+++ my-portfolio/src/pages/Project1/index.tsx
@@ -238,23 +238,85 @@ const Project1: React.FC = () => {
               Collaborators: Nicholas Hardy, Hassan Hijazi, Marybel Boujaoude, Riya Deokar, Jazmyn Walker
               <br />
               <br />
-              Date: 2023-09-01 - Present
+              Date: 2023-09-01 - 2024-05-01
               <br />
               <br />
               For my Senior Capstone Project, I chose to put forward a personal
               project rather than work for an existing company. This was where
-              EchoView.ai was born. EchoView.ai is a pair of augmented reality
-              glasses that are designed to help the deaf and hard of hearing
-              community.
+              EchoView.ai was born.
               <br /><br />
-              The glasses are designed to transcribe speech in real-time and
-              display it on the user&apos;s field of view. This is done by sending audio via Bluetooth
-              microphones on the glasses to then be transcribed by a speech-to-text API. We have also built an iOS app to work in tandem with the glasses to allow for the user to customize the text size, color, and allow them to access their conversation history.
-              <br /><br />
-              This project is still in the early stages of development as we have just finished our first prototype. We are currently working on improving the latency of the speech-to-text API and the overall design of the glasses.
-              <br /><br />
-              APRIL 2024 RELEASE DATE - STAY TUNED!
+              EchoView.ai is dedicated to helping unlock a world of communication, collaboration and human connection. We believe that everyone should hold the key to understanding spoken interactions, so we can foster community, learning, and innovation 
             </p>
+            <h2
+              className="text-2xl font-semibold text-center mb-4 mt-4"
+              style={{ color: textColor }}
+            >
+              Key Features
+            </h2>
+            <ul className="text-gray-600 pl-4 list-disc"
+            style={{ fontFamily: "'Nunito', serif" }}>
+              <li>
+                Real-time speech-to-text transcription displayed directly in the user&apos;s field of view. 
+              </li>
+              <li>
+                Directional Microphones to focus on the speaker&apos;s voice and reduce background noise.
+              </li>
+              <li>
+                Bluetooth connectivity to pair with smartphones and other devices.
+              </li>
+              <li>
+                Very low prototype cost ($28.60 unit price) to ensure affordability.
+              </li>
+              <li>
+                Over 90% accuracy in speech-to-text transcription until 70dB.
+              </li>
+            </ul>
+            <h2
+              className="text-2xl font-semibold text-center mb-4 mt-4"
+              style={{ color: textColor }}
+            >
+              Implementation Details
+            </h2>
+            <ul className="text-gray-600 pl-4 list-disc"
+            style={{ fontFamily: "'Nunito', serif" }}>
+              <li>
+                ESP32-C3 microcontroller chosen for its processing power,
+                wireless capabilities and efficiency. This was used to establish the BLE connection with the iOS application.
+              </li>
+              <li>
+                SSD1306 OLED display used to display the real-time transcription into a bird bath optical combining system to acheive required 20cm focal length.
+              </li>
+              <li>
+                iOS mobile application developed using Swift to connect to the AR glasses via BLE and process the transcription requests using the on device Siri module. 
+              </li>
+              <li>
+                Custom 3D printed housing that holds the ESP32-C3, OLED display, mirrors and microphone array. The battery is connected to the ESP-32 via a braided USB-C cable that is routed through the frame and out the back of the glasses.
+              </li>
+            </ul>
+            <h2
+              className="text-2xl font-semibold text-center mb-4 mt-4"
+              style={{ color: textColor }}
+            >
+              Supporting Artifacts
+            </h2>
+            <div className="w-full h-64 relative mb-8">
+                <Image
+                  src="/assets/EchoView Logo.png"
+                  alt="Logo"
+                  layout="fill"
+                  objectFit="contain"
+                  className="rounded-lg"
+                />
+            </div>
+            <div className="w-full h-64 relative mb-8">
+                <Image
+                  src="/assets/EchoView.ai flow chart.png"
+                  alt="Schematic"
+                  layout="fill"
+                  objectFit="contain"
+                  className="rounded-lg"
+                />
+              </div>
           </div>
         </div>
       </div>
